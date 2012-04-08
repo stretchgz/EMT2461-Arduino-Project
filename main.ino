62467f560ca028931b9c75480fe6d0565e845177
Wai Tsang <i@wdt.im>
2012-04-08 12:18:41 -0400

    1. RNG is using Arduino built-in function instead of C++ 2. onboard LED now blinks with user defined time 3. rearrange IF-condition in LOOP() 4. more variables declaration

--- main.ino
+++ main.ino
@@ -3,29 +3,22 @@
 
 	Description: Arduino for EMT2461 project
 
-	License: WTFPL (http://sam.zoy.org/wtfpl/)
+	License: WTFPL (http://sam.zoy.org/wtfpl/) 
 
-
-
-_____/\\\\\\\\\____________________________/\\\______________________________________________________        
- ___/\\\\\\\\\\\\\_________________________\/\\\______________________________________________________       
-  __/\\\/////////\\\________________________\/\\\___________________/\\\_______________________________      
-   _\/\\\_______\/\\\___/\\/\\\\\\\__________\/\\\____/\\\____/\\\__\///____/\\/\\\\\\________/\\\\\____     
-    _\/\\\\\\\\\\\\\\\__\/\\\/////\\\____/\\\\\\\\\___\/\\\___\/\\\___/\\\__\/\\\////\\\_____/\\\///\\\__    
-     _\/\\\/////////\\\__\/\\\___\///____/\\\////\\\___\/\\\___\/\\\__\/\\\__\/\\\__\//\\\___/\\\__\//\\\_   
-      _\/\\\_______\/\\\__\/\\\__________\/\\\__\/\\\___\/\\\___\/\\\__\/\\\__\/\\\___\/\\\__\//\\\__/\\\__  
-       _\/\\\_______\/\\\__\/\\\__________\//\\\\\\\/\\__\//\\\\\\\\\___\/\\\__\/\\\___\/\\\___\///\\\\\/___ 
-        _\///________\///___\///____________\///////\//____\/////////____\///___\///____\///______\/////_____
-
+	Notes:
+	1. Robot will not move at full speed for accurate detection of objects.
 
 */
 
-long 		Distance, // => distance between car and object
-			duration; // -> bounce back time when soundwave is hitting an object
-const int 	pingPin = 7, // pin7 for sensor
-			ledPIN = 13; // onboard LED pin
-short int 	i = 0,
-			RandomDirection; // random direction
+long 		Distance, 			// distance between car and object
+			duration; 			// bounce back time when soundwave is hitting an object
+const int 	pingPin = 7, 		// pin7 for sensor
+			ledPIN = 13; 		// onboard LED pin
+short int 	i = 0,				// for loop counter, starts at zero
+			RandomDirection,	// random direction
+			randomMotorPin,
+			hBridgePin1,
+			hBridgePin2;
 
 
 void setup() {
@@ -47,7 +40,7 @@ void setup() {
 		going to use a random number to decide direction 
 		when the car is turning
 	*/
-	srand ( time(NULL) );
+	randomSeed(analogRead(0));
 }
 
 // Main Loop
@@ -64,25 +57,21 @@ void loop() {
 	duration = pulseIn(pingPin, HIGH);
 	Distance = microsecondsToCentimeters(duration); // convert distance to centimeters
 
-
 	/*
-	choose a random direction by rolling a dice:
-		0 = turn left
-		1 = turn right
-	
-	this will make the robot more advanced instead of
-	keep turning in one direction whenever hit an object
+		randomly generate a number from zero to one
+			0 = left
+			1 = right
+		ref: http://arduino.cc/en/Reference/random
 	*/
-	RandomDirection = rand() % 2;
-
+	RandomDirection = random(2);
 	
 	/*
-	when sensor picks up something, fix motor speed & direction
+	When sensors pick up something, change motor speed & direction
 
-	An obstacle is:
-		3 ft away from robot, slow down (about 40% speed) and make a turn
+	When an obstacle is:
 		1 ft away from robot, stop immediately, then make a turn
-		3+ ft away from robot, move forward at full speed
+		3 ft away from robot, slow down to 50-60% and make a turn
+		3+ ft away from robot, move forward
 
 	Controlling Speed by passing parameters into function:
 	We give the code a simple number 1, 2 and 3.
@@ -90,18 +79,22 @@ void loop() {
 		At 2, car stop
 		At 3, car moves at full speed
 	*/
-	if ( Distance <= 90 ) // ~3 ft
+
+	if ( Distance <= 30 ) // ~1 ft
 	{
 		// CarSpeed(speed, direction);
+		blinkBoardLED(1, 100); // blink onboard LED every 100ms
 		CarSpeed(1, RandomDirection);
 	}
-	else if ( Distance < 30 ) // ~1 ft
+	else if ( Distance < 100 ) // ~3 ft
 	{
+		blinkBoardLED(1, 250); // blink LED every 250ms
 		CarSpeed(2, RandomDirection);
 	}
 	else
 	{
-		CarSpeed(3, RandomDirection);
+		blinkBoardLED(0, 0); // LED off
+		ForwardCar(0);
 	}
 }
 
@@ -112,19 +105,18 @@ Other Functions to be called in the Main Loop
 ---------------------------------------- */
 
 // Control car speed
-void CarSpeed(int speed, int RandomDirection){
+void CarSpeed( int ControlSpeed, int RandomDirection) {
 	
 	/*
 		grabs the parameters from function
 		set 1 to pin#5
 		set 2 to pin#6
 	*/
-	int RandomDirection;
 	if ( RandomDirection = 1 ) 
 	{
 		randomMotorPin = 5;
 	}
-	else if
+	else
 	{
 		randomMotorPin = 6;
 	}
@@ -132,27 +124,22 @@ void CarSpeed(int speed, int RandomDirection){
 	/*
 		Control Car
 	*/
-	if ( speed = 1 )
+	if ( ControlSpeed = 1 )
 	{
-		blink(ledPIN, 5, 500); // blink onboard LED 5x in 500 ms
-
-		// set both motor at slower speed
+		// reduce power on both motor pins
 		analogWrite(5, 180);
 		analogWrite(6, 180);
 		// wait 800ms
 		delay(800);
-		// a random motor will speed up
+		// speed up a random motor pin
 		analogWrite(randomMotorPin, 255);
-
-		// about 600ms, 
+		// wait 600ms 
 		delay(600);
-		// call function: move forward
-		MoveCarForward();
+		// mvoes forward
+		ForwardCar(0);
 	}
-	else if ( speed = 2 )
+	else if ( ControlSpeed = 2 )
 	{
-		blink(ledPIN, 10, 500); // blink LED 10x in 500 ms
-
 		// stop both motor
 		analogWrite(6, 0);
 		analogWrite(5, 0);
@@ -163,47 +150,52 @@ void CarSpeed(int speed, int RandomDirection){
 	}
 	else
 	{
-		// full speed manually
-		analogWrite(5, 255);
-		analogWrite(6, 255);
+		ForwardCar(0);
 	}
 }
 
-// Reverse motor
+// Reverse motor utilizes H-Bridge
 void ReserveCar() {
-	/*	use H-Bridge for moving forward/backward
-		simply pass voltage to:
-			pin1
-			pin2
-			pin3
-			pin4
-		need 4 pins for 2 motors
+	/*
+		H-Bridge 4 pins
+			1   2
+			3   4
+		From 	To	
+		1 		4 	= forward
+		2 		3 	= reverse
 	*/
 	digitalWrite( hBridgePin1, HIGH );
-	digitalWrite( hBridgePin2, LOW);
+	digitalWrite( hBridgePin2, LOW );
 
 	/***********************************
 
 		warning	: h-bridge pins havent decided yet...
 
 	***********************************/
+
 }
 
-// Making this a function for easier control...
-void MoveCarForward() {
+// Moves car forward
+void ForwardCar( int ReverseCurrent ) {
 	analogWrite(5, 255);
 	analogWrite(6, 255);
 }
 
 /*
-   Blink onboard LED when picks up an object from sensor
+   Light up the LED and blink
+   fast blinking 	= car is about to hit object
+   slow blinking  	= something is detected
+   no light 		= nothing is detected
 */
-void blink(int whatPin, int howManyTimes, int milliSecs) {
-	for ( i = 0; i < howManyTimes; i++) {
-		digitalWrite(whatPin, HIGH);
-		delay(milliSecs/2);
-		digitalWrite(whatPin, LOW);
-		delay(milliSecs/2);
+void blinkBoardLED( int LED_Switch, int blinkDelayTime ) {
+	if ( LED_Switch = 1 ) {
+		digitalWrite(ledPIN, HIGH); // lights up
+		delay(blinkDelayTime);
+		digitalWrite(ledPIN, LOW); // lights out
+		delay(blinkDelayTime);
+	}
+	else {
+		digitalWrite(ledPIN, LOW); // lights out, just in case it won't turn off
 	}
 }
 
